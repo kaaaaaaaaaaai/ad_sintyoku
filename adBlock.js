a198025dca1893e3c1530d9cf51da335ea9527bb
kaihot <hotsplusph@gmail.com>
2015-05-27 11:18:44 +0900

    コメント削除

--- adBlock.js
+++ adBlock.js
@@ -4,36 +4,18 @@
 //http://livedoor.blogimg.jp/hatima/imgs/b/e/beb9b8c2.png
 
 $(function(){
-		var adsRule = [$("[id^=ads]"),$("[id^=ox_]"),$("[id^='MA']"),$("[id^=imobi]"),$("iframe [id^=aswift]"),$("[id^=google_ads_iframe]"),$("iframe [id^=google_ads_iframe]"),$("[id^=aswift]"),$("[id^=google_ads_frame]")];
-
-	//var adsRule = [$("iframe [id^=aswift]"),$("[id^=google_ads_iframe]"),$("iframe [id^=google_ads_iframe]"),$("[id^=aswift]"),$("[id^=google_ads_frame]")];
-	// var ids2 = $("iframe [id^=aswift]").length;
-	// var ids3 = $("[id^=google_ads_iframe]").length;
-	// var ids4 = $("iframe [id^=google_ads_iframe]").length;
-	 var ids = $("body").find("[id^=aswift]").length;
-	// var ids5 = $("[id^=google_ads_frame]").length;
-	//alert(ids);
-
+	//追加しすぎて意味わかんない
+	var adsRule = [$("[id^=ads]"),$("[id^=ox_]"),$("[id^='MA']"),$("[id^=imobi]"),$("iframe [id^=aswift]"),$("[id^=google_ads_iframe]"),$("iframe [id^=google_ads_iframe]"),$("[id^=aswift]"),$("[id^=google_ads_frame]")];
+	var ids = $("body").find("[id^=aswift]").length;
 	$.each(adsRule, function(){
-		//alert($("body").find(this).length);
     	if($("body").find(this).length >= 0){
-    		//alert("ok");
     		$(this).each(function() {
 			    var _url = retUrl($(this).width(),$(this).height());
 			    $(this).html("<img src='" + _url + "' class='adb_img' style='max-height:"+$(this).height()+"px;max-width:"+$(this).width()+"px;'>");
 			});
-    		 
     	}
 	});
-
 });
-//alert("aaa2");
-// $("[id^=aswift]").each(function() {
-    
-//     var _url = retUrl($(this).width(),$(this).height());
-//     $(this).html("<img src='" + _url + "' class='hoge' alt='' >");
-// });
-
 
 function retUrl(w,h){
 	if (w <= 100){
